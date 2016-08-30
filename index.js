c0b9f73dcbde13a7b0f7fe981bb05b436fd8b67c
Mujtaba Al-Tameemi <mujtaba@Mujtabas-MacBook-Pro.local>
2016-08-30 07:50:11 -0700

    Added API routes for the film search and the customer search

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ let app = express();
 let debug = require('debug')('app');
 let models = require('./models');
 
+app.get('/film/:query', (req, res) => models.FilmMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
+app.get('/customer/:query', (req, res) => models.CustomerMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
+
 app.listen(3000, () => debug('app server started on port 3000'));
