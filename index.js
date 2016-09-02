324f01b02a748853376a766f0cb937d5bcc111c7
Mujtaba Al-Tameemi <mujtaba@Mujtabas-MacBook-Pro.local>
2016-09-02 05:53:50 -0700

    Updated node app to not exit on uncaught exceptions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let express = require('express');
 let app = express();
 let models = require('./models');
 const PORT = process.env.PORT || 3000;
+process.on('uncaughtException', console.error);
 
 app.get('/film/:query', (req, res) => models.FilmMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
 app.get('/customer/:query', (req, res) => models.CustomerMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
