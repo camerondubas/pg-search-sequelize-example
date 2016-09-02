00c69f8fcab0790070765ac3e702ee430c2a8929
Mujtaba Al-Tameemi <mujtaba@Mujtabas-MacBook-Pro.local>
2016-09-01 18:46:03 -0700

    Express server uses PORT env var if it is found

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 let express = require('express');
 let app = express();
 let models = require('./models');
+const PORT = process.env.PORT || 3000;
 
 app.get('/film/:query', (req, res) => models.FilmMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
 app.get('/customer/:query', (req, res) => models.CustomerMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
 
-app.listen(3000, () => console.log('app server started on port 3000'));
+app.listen(PORT, () => console.log('app server started on port ' + PORT));
