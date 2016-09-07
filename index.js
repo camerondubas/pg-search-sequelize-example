6f8d18c6ab87c66bd49e6b5c7fd4002f82e66a54
Cameron Dubas <camerondubas@gmail.com>
2016-09-06 22:12:58 -0700

    Allow CORS

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ let models = require('./models');
 const PORT = process.env.PORT || 3000;
 process.on('uncaughtException', console.error);
 
+let allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 app.get('/film/:query', (req, res) => models.FilmMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
 app.get('/customer/:query', (req, res) => models.CustomerMaterializedView.searchByText(req.params.query).then(data => res.send(data)));
 
